31055186a692fd7974cf0c116826da2315bc44b1
Kerry Nasta <KerryNasta@kerrys-macbook-pro.local>
2018-04-17 10:28:31 -0400

    Nasta Snacktacular

---
+++ Snacktacular/Spot.swift
@@ -0,0 +1,103 @@
+//
+//  Spot.swift
+//  Snacktacular
+//
+//  Created by Kerry Nasta on 4/15/18.
+//  Copyright © 2018 John Gallaugher. All rights reserved.
+//
+
+import Foundation
+import CoreLocation
+import Firebase
+import MapKit
+
+class Spot: NSObject, MKAnnotation {
+    var name: String
+    var address: String
+    var coordinate: CLLocationCoordinate2D
+    var averageRating: Double
+    var numberOfReviews: Int
+    var postingUserID: String
+    var documentID: String
+    
+    var longitude: CLLocationDegrees {
+        return coordinate.longitude
+    }
+    var latitude: CLLocationDegrees {
+        return coordinate.latitude
+    }
+    
+    var title: String? {
+        return name
+    }
+    
+    var subtitle: String? {
+        return address
+    }
+    
+    var dictionary: [String: Any] {
+        return ["name": name, "address": address, "longitude": longitude, "latitude": latitude, "averageRating": averageRating, "numberOfReviews": numberOfReviews, "postingUserID": postingUserID]
+    }
+    
+    init(name: String, address: String, coordinate: CLLocationCoordinate2D, averageRating: Double, numberOfReviews:Int, postingUserID: String, documentID: String) {
+        self.name = name
+        self.address = address
+        self.coordinate = coordinate
+        self.averageRating = averageRating
+        self.numberOfReviews = numberOfReviews
+        self.postingUserID = postingUserID
+        self.documentID = documentID
+    }
+    
+    convenience override init() {
+        self.init(name: "", address: "", coordinate: CLLocationCoordinate2D(), averageRating: 0.0, numberOfReviews: 0, postingUserID: "", documentID: "")
+    }
+    
+    convenience init(dictionary: [String: Any]) {
+        let name = dictionary["name"] as! String? ?? ""
+        let address = dictionary["address"] as! String? ?? ""
+        let latitude = dictionary["latitude"] as! CLLocationDegrees? ?? 0.0
+        let longitude = dictionary["longitude"] as! CLLocationDegrees? ?? 0.0
+        let coordinate = CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
+        let averageRating = dictionary["averageRating"] as! Double? ?? 0.0
+        let numberOfReviews = dictionary["numberOfReviews"] as! Int? ?? 0
+        let postingUserID = dictionary["postingUserID"] as! String? ?? ""
+        self.init(name: name, address: address, coordinate: coordinate, averageRating: averageRating, numberOfReviews: numberOfReviews, postingUserID: postingUserID, documentID: "")
+    }
+    
+    func saveData(completed: @escaping (Bool) -> ()) {
+        let db = Firestore.firestore()
+        //Grab the UserID
+        guard let postingUserID = (Auth.auth().currentUser?.uid) else {
+            print("***ERROR")
+            return completed(false)
+        }
+        self.postingUserID = postingUserID
+        //create the dictionary representing the data we want to save
+        let dataToSave = self.dictionary
+        //if we hAVE saved a record, we'll have a documentID
+        if self.documentID != "" {
+            let ref = db.collection("spots").document(self.documentID)
+            ref.setData(dataToSave) { (error) in
+                if let error = error {
+                    print("***ERROR")
+                    completed(false)
+                } else {
+                    print("^^^document updated with ref ID")
+                    completed(true)
+                }
+            }
+        } else {
+            var ref: DocumentReference? = nil
+            ref = db.collection("spots").addDocument(data: dataToSave) { error in
+                if let error = error {
+                    print("***ERROR")
+                    completed(false)
+                } else {
+                    print("^^^new document created with ref ID")
+                    completed(true)
+                }
+            }
+        }
+    }
+}
